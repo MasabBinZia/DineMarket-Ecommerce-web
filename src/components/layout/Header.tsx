5b64fad71df8f71d55ba35ea6fe3c4388da24ca3
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-06-13 10:40:39 +0500

    Menu ,X added for smallScreens

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,45 +1,54 @@
-import React from 'react'
-import Image from 'next/image'
-import logo from '/public/logo.webp'
-import Link from 'next/link'
-import { Input } from "../ui/input"
-import { ShoppingCart } from "lucide-react"
-import { Search } from "lucide-react"
-
+"use client"
+import React, { useState } from 'react';
+import Image from 'next/image';
+import logo from '/public/logo.webp';
+import Link from 'next/link';
+import { Input } from "../ui/input";
+import { ShoppingCart } from "lucide-react";
+import { Menu, X ,Search} from "lucide-react";
 
 const Header = () => {
-    return (
-        
-        <nav className='flex justify-between items-center mt-8 ml-32 mr-32 '>
-            <div className='logo'>
-            <Image src={logo} alt='logo' className='w-40' />
-            </div>
-            <div className="navlinks">
-            <ul className='flex gap-x-10 '>
-                <li className='text-lg'>
-                    <Link href={"/"}>Females</Link>
-                </li>
-                <li className='text-lg'>
-                    <Link href={"/"}>Males</Link>
-                </li>
-                <li className='text-lg'>
-                    <Link href={"/"}>Kids</Link>
-                </li>
-                <li className='text-lg'>
-                    <Link href={"/"}>All Products</Link>
-                </li>
-            </ul>
-            </div>
-            <div className="search-bar flex items-center pt-0">
-            <Search className="bg-white rounded-l" />
-         <Input type="search" placeholder="Search Here!" className='w-60 h-6 rounded' />
-         </div>
-            <div className=' flex h-10 w-10 rounded-full bg-gray-200 justify-center  items-center'>
-                <ShoppingCart />
-            </div>
-        </nav>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    )
-}
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-export default Header
+  return (
+    <nav className='flex justify-between items-center mt-8 mx-4 sm:mx-8 md:mx-16 lg:mx-32'>
+      <div className='logo'>
+        <Image src={logo} alt='logo' className='w-40' />
+      </div>
+      <div className='md:hidden'>
+        {isMenuOpen ? (
+          <X
+            className='text-gray-500 cursor-pointer'
+            size={24}
+            onClick={handleMenuToggle}
+          />
+        ) : (
+          <Menu
+            className='text-gray-500 cursor-pointer'
+            size={24}
+            onClick={handleMenuToggle}
+          />
+        )}
+      </div>
+      <div className={`navlinks ${isMenuOpen ? 'flex' : 'hidden'} md:flex flex-col md:flex-row gap-x-10`}>
+        <Link href={"/"} className='text-lg'> Females</Link>
+        <Link href={"/"} className='text-lg'>Males</Link>
+        <Link href={"/"} className='text-lg'>Kids</Link>
+        <Link href={"/"}className='text-lg'>All Products</Link>
+      </div>
+      <div className="search-bar flex items-center pt-0">
+        <Search className="bg-white rounded-l" />
+        <Input type="search" placeholder="Search Here!" className='w-60 h-6 rounded' />
+      </div>
+      <div className='flex h-10 w-10 rounded-full bg-gray-200 justify-center items-center'>
+        <ShoppingCart />
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
