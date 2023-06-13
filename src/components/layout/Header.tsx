91f66a6af2f9d9793eaf4dd76f593419a36f4bbd
Masab Bin Zia <masabmbz5@gmail.com>
2023-06-14 03:29:26 +0500

    fixing-4

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -16,40 +16,78 @@ const Header = () => {
     setIsSearchOpen(false);
   };
 
-  
-
   return (
-    <nav className='flex justify-between items-center mt-8 mx-4 sm:mx-8 md:mx-16 lg:mx-32'>
-      <div className='logo'>
-        <Image src={logo} alt='logo' className='w-40' />
-      </div>
-      <div className='md:hidden'>
-        {isMenuOpen ? (
-          <X
-            className='text-gray-500 cursor-pointer'
-            size={24}
-            onClick={handleMenuToggle}
-          />
-        ) : (
-          <Menu
-            className='text-gray-500 cursor-pointer'
-            size={24}
-            onClick={handleMenuToggle}
-          />
-        )}
-      </div>
-      <div className={`navlinks ${isMenuOpen ? 'flex' : 'hidden'} md:flex flex-col md:flex-row gap-x-10`}>
-        <Link href={"/"} className='text-lg'>Females</Link>
-        <Link href={"/"} className='text-lg'>Males</Link>
-        <Link href={"/"} className='text-lg'>Kids</Link>
-        <Link href={"/"} className='text-lg'>All Products</Link>
-      </div>
-      <div className={`${isSearchOpen ? 'flex' : 'hidden'} md:flex items-center pt-0`}>
-        <Search className="bg-white rounded-l" />
-        <Input type="search" placeholder="Search Here!" className='w-60 h-6 rounded' />
-      </div>
-      <div className={` ${isMenuOpen ? 'flex' : 'hidden'} md:flex h-10 w-10 rounded-full bg-gray-200 justify-center items-center cursor-pointer`}>
-        <ShoppingCart />
+    <nav>
+      {/* Large Devices */}
+      <div className='flex justify-between items-center mt-8 mx-4 sm:mx-8 md:mx-16 lg:mx-32'>
+        <div className='logo'>
+          <Image src={logo} alt='logo' className='w-40' />
+        </div>
+        <div className='md:hidden'>
+          {isMenuOpen ? (
+            <X
+              className='text-gray-500 cursor-pointer'
+              size={24}
+              onClick={handleMenuToggle}
+            />
+          ) : (
+            <Menu
+              className='text-gray-500 cursor-pointer'
+              size={24}
+              onClick={handleMenuToggle}
+            />
+          )}
+        </div>
+        <div
+          className={`hidden md:flex flex-col md:flex-row gap-x-10 `}
+        >
+          <Link href={"/"} className='text-lg'>
+            Females
+          </Link>
+          <Link href={"/"} className='text-lg'>
+            Males
+          </Link>
+          <Link href={"/"} className='text-lg'>
+            Kids
+          </Link>
+          <Link href={"/"} className='text-lg'>
+            All Products
+          </Link>
+        </div>
+        <div
+          className={`${isSearchOpen ? 'flex' : 'hidden'
+            } md:flex items-center pt-0`}
+        >
+          <Search className="bg-white rounded-l" />
+          <Input type="search" placeholder="Search Here!" className='w-60 h-6 rounded' />
+        </div>
+        <div className={`hidden md:flex h-10 w-10 rounded-full bg-gray-200 justify-center items-center cursor-pointer`}
+        >
+          <ShoppingCart />
+        </div>
+        {/* Mobile Devices */}
+        <div
+          className={`${isMenuOpen
+              ? "flex lg:hidden flex-col w-full h-screen items-center fixed top-0 left-0 z-[100] bg-white px-5 "
+              : "flex lg:hidden flex-col w-full h-screen items-center fixed top-[-100%] left-0 z-[100] bg-white px-5 "
+            }`}
+        >
+          <div className=''>
+          <div
+            className={`navlinks ${isMenuOpen ? 'flex' : 'hidden'} md:flex flex-col md:flex-row gap-x-10`}
+          >
+            <Link href={"/"} className='text-lg'>Females</Link>
+            <Link href={"/"} className='text-lg'>Males</Link>
+            <Link href={"/"} className='text-lg'>Kids</Link>
+            <Link href={"/"} className='text-lg'>All Products</Link>
+          </div>
+          <div
+            className={` ${isMenuOpen ? 'flex' : 'hidden'} md:flex h-10 w-10 rounded-full bg-gray-200 justify-center items-center cursor-pointer`}
+          >
+            <ShoppingCart />
+          </div>
+          </div>
+        </div>
       </div>
     </nav>
   );
--- src/views/ProductList.tsx
+++ src/views/ProductList.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 
 const ProductList = () => {
   return (
-    <div className='flex justify-evenly mt-16'>
+    <div className='flex justify-evenly mt-16 p-0 ml-auto mr-auto z-1  '>
        <ProductCard tittle='Flex Sweatshirt' price={175} img={P1}/>
        <ProductCard tittle='Cameryn Sash Tie Dress' price={545} img={P3}/>
        <ProductCard tittle='Brushed Raglan Sweatshirt' price={195} img={P3}/>
