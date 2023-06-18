d9444309dbe698cbc7b0cee5800b18bf22dd5313
Masab Bin Zia <masabmbz5@gmail.com>
2023-06-18 06:14:15 +0500

    fixed Newsletter text bar

--- src/views/NewsLatter.tsx
+++ src/views/NewsLatter.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 const Newslatter = () => {
     return (
@@ -7,7 +8,7 @@ const Newslatter = () => {
             <h6 className="text-3xl md:text-4xl font-bold">Subscribe Our Newsletter</h6>
             <p className="max-w-[16rem] text-lg font-medium">Get the latest information and promo offers directly</p>
             <div className="flex gap-4 flex-wrap items-center justify-center ">
-                <input type="text" className="border border-gray-600 py-1 px-2 md:px-4 flex flex-grow w-80" placeholder="Input email address" />
+                <Input type="text" className="border border-gray-600 py-1 px-2 md:px-4 flex flex-grow w-80" placeholder="Input email address" />
                 <Button>Get Started </Button>
             </div>
         </div>
