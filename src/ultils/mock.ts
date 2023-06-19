e92ffa2a24d273aca8695d1ce40ce5fe0acdb1d1
Masab Bin Zia <masabmbz5@gmail.com>
2023-06-20 02:40:52 +0500

    add utils

---
+++ src/ultils/mock.ts
@@ -0,0 +1,47 @@
+import { Product } from "./types";
+import P1 from '/public/p1.webp'
+
+const products:Product []=[
+{
+    id:1,
+    name:'Product 1',
+    category:'male',
+    price:200,
+    image:P1,
+},
+{
+    id:2,
+    name:'Product 2',
+    category:'female',
+    price:200,
+    image:P1,
+},
+{
+    id:3,
+    name:'Product 3',
+    category:'male',
+    price:200,
+    image:P1,
+},
+{
+    id:4,
+    name:'Product 4',
+    category:'Kids',
+    price:200,
+    image:P1,
+},
+{
+    id:5,
+    name:'Product 5',
+    category:'kids',
+    price:200,
+    image:P1,
+},
+{
+    id:6,
+    name:'Product 6',
+    category:'male',
+    price:200,
+    image:P1,
+}
+];
