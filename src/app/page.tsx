2853df40ec72977f72840d2f8e008df88e171f14
Masab Bin Zia <masabmbz5@gmail.com>
2023-06-27 22:09:01 +0500

    Promotion-done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <>
       <div className='py-0 ml-0 lg:py-12 lg:ml-20'>
         <Hero />
-        {/* <Promotion/> */}
+        <Promotion/>
         <ProductList />
         <Jewellery />
         <Newslatter />
