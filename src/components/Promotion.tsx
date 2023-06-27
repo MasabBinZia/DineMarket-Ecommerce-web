2e2d232a3bc5a0e62b822ce5a1e21a45e28cc19c
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-06-27 16:50:52 +0500

    productList update

---
+++ src/components/Promotion.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Promotion = () => {
+  return (
+    <div>
+        <div className="text-center space-y-3">
+        <p className="text-blue-800 text-sm font-bold">PROMOTIONS</p>
+        <h3 className="text-3xl text-gray-800 font-bold">Our Promotions Events</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Promotion
--- src/views/ProductList.tsx
+++ src/views/ProductList.tsx
@@ -8,8 +8,8 @@ const ProductList = () => {
   return (
     <div className='mt-8'>
       <div className="text-center space-y-3">
-        <p className="text-blue-800 text-sm font-bold">PROMOTIONS</p>
-        <h3 className="text-3xl text-gray-800 font-bold">Our Promotions Events</h3>
+        <p className="text-blue-800 text-sm font-bold">PRODUCTS</p>
+        <h3 className="text-3xl text-gray-800 font-bold">Check What We Have</h3>
       </div>
       <div className='flex flex-col md:flex-row justify-center md:justify-evenly mt-8 md:mt-16 p-4 md:p-0'>
         {productsChecks.map((products) => (
