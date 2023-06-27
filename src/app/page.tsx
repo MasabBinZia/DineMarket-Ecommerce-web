d4c33454a279ce5ba9d4bedf19091d73e380d1bf
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-06-27 17:25:25 +0500

    promo adding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Hero from '@/views/Hero'
 import Jewellery from '@/components/Jewellery'
 import Newslatter from '@/components/NewsLatter'
 import ProductList from '@/views/ProductList'
+import Promotion from '@/components/Promotion'
 
 export default function Home() {
   return (
     <>
     <div className='py-0 ml-0 lg:py-12 lg:ml-20'>
     <Hero/>
+    {/* <Promotion/> */}
     <ProductList/>
     <Jewellery/>
     <Newslatter/>
--- src/components/Promotion.tsx
+++ src/components/Promotion.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import Image from "next/image";
 
 const Promotion = () => {
   return (
-    <div>
+    <div className='mt-8'>
         <div className="text-center space-y-3">
         <p className="text-blue-800 text-sm font-bold">PROMOTIONS</p>
-        <h3 className="text-3xl text-gray-800 font-bold">Our Promotions Events</h3>
+        <h1 className="text-3xl text-gray-800 font-bold">Our Promotions Events</h1>
+      </div>
+      <div className='flex flex-col flex-grow gap-4'>
+        {/* 60% OFF */}
+        <div className='h-40 px-3  flex flex-row justify-between item-center bg-gray-400'>
+            <h2 className='block font-semibold text-xl'>GET UP TO <span className='text-3xl'>60%</span></h2>
+            <p className='block'>for the summer season</p>
+        </div>
+        <div className="Img">
+            {/* <Image src={} /> */}
+        </div>
       </div>
     </div>
   )
