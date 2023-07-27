b53a380391836758496e311ce3f4dd4258ff8a41
Masab Bin Zia <masabmbz5@gmail.com>
2023-07-27 20:30:08 +0500

    responsiveness fixes

--- src/components/Promotion.tsx
+++ src/components/Promotion.tsx
@@ -14,39 +14,33 @@ const Promotion = () => {
           Our Promotions Events
         </h1>
       </div>
-      <div className="flex flex-col md:flex md:flex-row gap-4 item-center justify-center">
+      <div className="flex flex-col lg:flex lg:flex-row gap-4 item-center justify-center">
         {/* Left */}
-        <div className="flex items-center md:flex flex-col gap-4">
+        <div className="flex items-center lg:flex flex-col gap-4">
           {/*60% off*/}
-          <div className="w-[400px] md:h-50 md:w-[600px] px-8 flex flex-row rounded justify-between items-center bg-gray-300">
+          <div className="w-[300px] lg:h-50 lg:w-[600px] md:w-[600px] px-8 flex flex-row rounded justify-between items-center bg-gray-300">
             <div>
-              <h2 className="block font-bold text-2xl">
-                GET UP TO <span className="text-4xl">60%</span>
+              <h2 className="block font-bold lg:text-2xl md:text-2xl text-sm">
+                GET UP TO{" "}
+                <span className="text-2xl lg:text-4xl md:text-4xl">60%</span>
               </h2>
               <p className="block">For the summer season</p>
             </div>
-            <div className="Img">
-              <Image
-                src={Promo1}
-                alt="Promotion Girl"
-                width={260}
-                height={200}
-              />
-            </div>
+            <Image src={Promo1} alt="Promotion Girl" width={260} height={200} />
           </div>
           {/*30% off*/}
-          <div className="w-[400px] md:h-52 md:w-[600px] flex flex-col items-center rounded justify-center gap-y-3 bg-zinc-800">
+          <div className="w-[300px] lg:h-52 lg:w-[600px] md:w-[600px] flex flex-col items-center rounded justify-center gap-y-3 bg-zinc-800">
             <h2 className="text-4xl text-white font-bold mt-8">GET 30% OFF</h2>
             <p className="text-sm text-white">USE PROMO CODE</p>
-            <Button className="mb-4 md:text-lg tracking-widest text-white bg-zinc-700 px-4 py-1 rounded-lg hover:bg-zinc-700">
+            <Button className="mb-4 lg:text-lg tracking-widest text-white bg-zinc-700 px-4 py-1 rounded-lg hover:bg-zinc-700">
               DINEWEEKENDSALE
             </Button>
           </div>
         </div>
         {/* Right */}
-        <div className="flex flex-col justify-between items-center gap-4 md:flex md:flex-row">
+        <div className="flex flex-col md:flex-row justify-center lg:justify-between items-center gap-4 lg:flex lg:flex-row">
           <div
-            className="flex flex-col justify-between items-center rounded  pb-0 pt-3 px-2 w-80 md:w-56 h-[425px]"
+            className="flex flex-col justify-between items-center rounded  pb-0 pt-3 px-2  lg:w-56 h-[425px]"
             style={{ backgroundColor: "#efe1c7" }}
           >
             <div>
@@ -68,7 +62,7 @@ const Promotion = () => {
               />
             </div>
           </div>
-          <div className="flex flex-col justify-between items-center rounded pb-0 pt-3 px-2 w-80 md:w-56 h-[425px] bg-gray-300">
+          <div className="flex flex-col justify-between items-center rounded pb-0 pt-3 px-2  lg:w-56 h-[425px] bg-gray-300">
             <div>
               <h2 className={"text-md"}>
                 Flex Push Button Bomber
