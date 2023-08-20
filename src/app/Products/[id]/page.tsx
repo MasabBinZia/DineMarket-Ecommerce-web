ac16ec491e841597b6451e314ebd912287dec984
Masab Bin Zia <masabmbz5@gmail.com>
2023-08-21 03:20:28 +0500

    fixing

--- src/app/Products/[id]/page.tsx
+++ src/app/Products/[id]/page.tsx
@@ -1,15 +1,9 @@
-import Quantity from "@/components/Quantity";
-import { Button } from "@/components/ui/button";
-import { products } from "@/ultils/productMockData";
-import { ShoppingCart } from "lucide-react";
-import Image from "next/image";
 import IdPage from "./idPage";
-
 
 export default function Page({ params }: { params: { id: string } }) {
   return (
-   <>
-   <IdPage params={params}/>
-   </>
+    <>
+      <IdPage params={params} />
+    </>
   );
 }
--- src/app/Products/page.tsx
+++ src/app/Products/page.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ProductPage from "./productPage";
 
 const AllProducts = () => {
