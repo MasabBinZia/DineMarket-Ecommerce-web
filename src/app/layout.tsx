684bf1feb96559152169d544c0f81141509cbe8c
Masab Bin Zia <masabmbz5@gmail.com>
2023-08-21 02:30:27 +0500

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import Credits from "@/components/layout/Credits";
-import Providers from "./providers";
+import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
